ada87f45829687a01ff7e2ce2ae81bf156c0035a
Axel Obscura <axosar@gmail.com>
2020-06-05 09:44:32 -0500

    Scales - Linear, Logarithmic, Time, Ordinal, Band

--- js/main.js
+++ js/main.js
@@ -1,37 +1,51 @@
 //var data = [25, 20, 10, 12, 15];
 
+var svg = d3.select("#buildings").append("svg")
+    .attr("width", 400)
+    .attr("height", 400);
+
 d3.json("data/buildings.json").then(function (data) {
+
+    console.log(data);
+
     data.forEach((d) => {
         d.height = +d.height;
     });
+
+    var x = d3.scaleBand()
+        .domain([
+            "Burj Khalifa",
+            "Shanghai Tower",
+            "Abraj Al-Bait Clock Tower",
+            "Ping An Finance Centre",
+            "Lotte World Tower",
+            "Hotel de México"
+        ])
+        .range([0, 400])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
 
     var y = d3.scaleLinear()
         .domain([0, 828])
         .range([0, 400]);
 
-    var svg = d3.select("#buildings").append("svg")
-        .attr("width", 400)
-        .attr("height", 400);
-
     var rects = svg.selectAll("rect")
-        .data(data);
-
-    rects.enter()
+        .data(data)
+        .enter()
         .append("rect")
-        .attr("x", function (d, i) {
-            console.log(d);
-            return (i * 60);
+        .attr("y", 20)
+        .attr("x", function (d) {
+            return x(d.name)
         })
-        .attr("y", 0)
-        .attr("width", 55)
+        .attr("width", x.bandwidth)
         .attr("height", function (d) {
             return y(d.height);
         })
         .attr("fill", function (d) {
-            return "grey";
+            return "red";
         });
 });
-
+/*
 d3.tsv("data/ages.tsv").then(function (data) {
     data.forEach(function (d) {
         // Converting values to integers
